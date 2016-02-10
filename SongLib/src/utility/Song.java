f25eb7c293d56d81acb003f1806a6dbb6418f9f4
aznkid66 <aznkid66@gmail.com>
2016-02-10 12:50:06 -0500

    song

---
+++ SongLib/src/utility/Song.java
@@ -0,0 +1,25 @@
+package utility;
+
+import java.util.Comparator;
+
+public class Song {
+	public static class Compare implements Comparator<Song> {
+
+		@Override
+		public int compare(Song o1, Song o2) {
+			return o1.name.compareTo(o2.name);
+		}
+		
+	}
+	public String name;
+	public String artist;
+	
+	public Song (String name, String artist) {
+		this.name = name;
+		this.artist = artist;
+	}
+	
+	public String toString() {
+		return name;
+	}
+}
